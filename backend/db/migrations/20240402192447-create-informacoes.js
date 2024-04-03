dce9c4ee23bdc6ced74a476c55381f57ccaddf13
Dianne Faria de Brito <diannefaria09@gmail.com>
2024-04-03 08:46:10 -0300

    Criação backend upload arquivo xlsx

---
+++ backend/db/migrations/20240402192447-create-informacoes.js
@@ -0,0 +1,55 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Informacoes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Data_da_venda: {
+        type: Sequelize.STRING
+      },
+      Vendedor: {
+        type: Sequelize.STRING
+      },
+      CPF_Vendedor: {
+        type: Sequelize.STRING
+      },
+      Produto: {
+        type: Sequelize.STRING
+      },
+      ID_Produto: {
+        type: Sequelize.STRING
+      },
+      Cliente: {
+        type: Sequelize.STRING
+      },
+      CNPJ_CPF_Cliente: {
+        type: Sequelize.STRING
+      },
+      Segmento_do_Cliente: {
+        type: Sequelize.STRING
+      },
+      Valor_de_Venda: {
+        type: Sequelize.STRING
+      },
+      Forma_de_Pagamento: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Informacoes');
+  }
+};
---
+++ backend/db/models/informacoes.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Informacoes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Informacoes.init({
+    Data_da_venda: DataTypes.STRING,
+    Vendedor: DataTypes.STRING,
+    CPF_Vendedor: DataTypes.STRING,
+    Produto: DataTypes.STRING,
+    ID_Produto: DataTypes.STRING,
+    Cliente: DataTypes.STRING,
+    CNPJ_CPF_Cliente: DataTypes.STRING,
+    Segmento_do_Cliente: DataTypes.STRING,
+    Valor_de_Venda: DataTypes.STRING,
+    Forma_de_Pagamento: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Informacoes',
+  });
+  return Informacoes;
+};

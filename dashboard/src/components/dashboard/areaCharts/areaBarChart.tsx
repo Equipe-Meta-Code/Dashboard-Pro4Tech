5c56d9e9746561637c550a4c605387e5e0a64c29
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-04-11 08:38:45 -0300

    Update areaBarChart.tsx

--- dashboard/src/components/dashboard/areaCharts/areaBarChart.tsx
+++ dashboard/src/components/dashboard/areaCharts/areaBarChart.tsx
@@ -1,67 +1,38 @@
-import {
-  Area,
-  AreaChart,
-  CartesianGrid,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
-import "./AreaCharts.scss";
 
-const data = [
-  //exemplo de dados
-  {
-    mes: "Jan",
-    sales: 70,
-  },
-  {
-    mes: "Fev",
-    sales: 55,
-  },
-  {
-    mes: "Mar",
-    sales: 35,
-  },
-  {
-    mes: "Abril",
-    sales: 90,
-  },
-  {
-    mes: "Maio",
-    sales: 55,
-  },
-  {
-    mes: "Jun",
-    sales: 49,
-  },
-  {
-    mes: "Jul",
-    sales: 32,
-  },
-  {
-    mes: "Ago",
-    sales: 62,
-  },
-  {
-    mes: "Set",
-    sales: 87,
-  },
-  {
-    mes: "Out",
-    sales: 55,
-  },
-  {
-    mes: "Nov",
-    sales: 85,
-  },
-  {
-    mes: "Dez",
-    sales: 108,
-  },
-];
+import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import axios from 'axios';
 
 const AreaBarChart = () => {
+  const [chartData, setChartData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      // Definindo os nomes dos meses
+      const monthNames = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+
+      const response = await axios.get('http://localhost:8080/dados_vendas_mes');
+      const data = response.data.map(item => {
+        // Convertendo o número do mês para o nome completo do mês
+        const monthIndex = item.mes - 1; // Mês em JavaScript é baseado em zero
+        const monthName = monthNames[monthIndex];
+        return { ...item, mes: monthName };
+      });
+      // Ordenando os dados por ordem dos meses
+      data.sort((a, b) => monthNames.indexOf(a.mes) - monthNames.indexOf(b.mes));
+      setChartData(data);
+      setLoading(false);
+    } catch (error) {
+      console.error('Erro ao buscar dados:', error);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="bar-chart">
       <div className="bar-chart-info">
@@ -72,28 +43,36 @@ const AreaBarChart = () => {
       </div>
       <div className="bar-chart-wrapper">
         <ResponsiveContainer width="100%" height="100%">
-          <AreaChart
-            width={500}
-            height={400}
-            data={data}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="mes" />
-            <YAxis />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="sales"
-              stroke="#a9dfd8"
-              fill="#a9dfd8"
-            />
-          </AreaChart>
+          {loading ? (
+            <p>Carregando...</p>
+          ) : (
+            chartData.length > 0 ? (
+              <AreaChart
+                width={500}
+                height={400}
+                data={chartData}
+                margin={{
+                  top: 10,
+                  right: 30,
+                  left: 0,
+                  bottom: 0,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="mes" />
+                <YAxis />
+                <Tooltip />
+                <Area
+                  type="monotone"
+                  dataKey="total_vendas"
+                  stroke="#a9dfd8"
+                  fill="#a9dfd8"
+                />
+              </AreaChart>
+            ) : (
+              <p>Não há dados disponíveis.</p>
+            )
+          )}
         </ResponsiveContainer>
       </div>
     </div>

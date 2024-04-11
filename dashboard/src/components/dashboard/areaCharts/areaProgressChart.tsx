b4ac56166c97710c1e039209ddffac97ce43730d
Sofia Lessa <126245999+sofialessaa@users.noreply.github.com>
2024-04-10 23:24:11 -0300

    Update areaProgressChart.tsx

--- dashboard/src/components/dashboard/areaCharts/areaProgressChart.tsx
+++ dashboard/src/components/dashboard/areaCharts/areaProgressChart.tsx
@@ -1,51 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import "./AreaCharts.scss";
 
-const data = [
-  //exemplo de dados
-  {
-    id: 1,
-    name: "Produto 1",
-    percentValues: 70,
-  },
-  {
-    id: 2,
-    name: "Produto 2",
-    percentValues: 40,
-  },
-  {
-    id: 3,
-    name: "Produto 3",
-    percentValues: 60,
-  },
-];
-
 const AreaProgressChart = () => {
+  const [chartData, setChartData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [total, setTotal] = useState(0); // Adicionando o estado para o total
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/dados_itens');
+      const data = response.data;
+      setChartData(data);
+      setLoading(false);
+
+      // Calculando o total
+      const totalVendido = data.reduce((acc, item) => acc + item.quantidade_vendida, 0);
+      setTotal(totalVendido);
+    } catch (error) {
+      console.error('Erro ao buscar dados:', error);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="progress-bar">
       <div className="progress-bar-info">
         <h4 className="progress-bar-title">Itens Mais Vendidos</h4>
       </div>
       <div className="progress-bar-list">
-        {data?.map((progressbar) => {
-          return (
-            <div className="progress-bar-item" key={progressbar.id}>
-              <div className="bar-item-info">
-                <p className="bar-item-info-name">{progressbar.name}</p>
-                <p className="bar-item-info-value">
-                  {progressbar.percentValues}
-                </p>
+        {loading ? (
+          <p>Carregando...</p>
+        ) : (
+          chartData.length > 0 ? (
+            chartData.map((item) => (
+              <div className="progress-bar-item" key={item.id}>
+                <div className="bar-item-info">
+                  <p className="bar-item-info-name">{item.Produto}</p>
+                  <p className="bar-item-info-value">
+                    {item.quantidade_vendida} ({((item.quantidade_vendida / total) * 100).toFixed(2)}% do total)
+                  </p>
+                </div>
+                <div className="bar-item-full">
+                  <div
+                    className="bar-item-filled"
+                    style={{
+                      width: `${(item.quantidade_vendida / total) * 100}%`, // Convertendo para porcentagem
+                    }}
+                  ></div>
+                </div>
               </div>
-              <div className="bar-item-full">
-                <div
-                  className="bar-item-filled"
-                  style={{
-                    width: `${progressbar.percentValues}%`,
-                  }}
-                ></div>
-              </div>
-            </div>
-          );
-        })}
+            ))
+          ) : (
+            <p>Não há dados disponíveis.</p>
+          )
+        )}
       </div>
     </div>
   );
